4d86680b223c7959f63fbee1cc84fc786ec70eb8
Jatintiwari3341 <tripathijatin319@gmail.com>
2025-04-29 00:54:56 +0530

    TLl

--- Proj01/src/main/java/com/nt/main/App.java
+++ Proj01/src/main/java/com/nt/main/App.java
@@ -6,5 +6,7 @@ package com.nt.main;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("task 1");
+        
     }
 }
