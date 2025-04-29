91bbf2e617aeeeb5571b920010d1ea311cf7630c
Jatintiwari3341 <tripathijatin319@gmail.com>
2025-04-29 23:18:22 +0530

    TL create project

--- Proj01/src/main/java/com/nt/main/App.java
+++ Proj01/src/main/java/com/nt/main/App.java
@@ -4,9 +4,14 @@ package com.nt.main;
  * Hello world!
  */
 public class App {
+	
+	public int Sum(int a,int b) {
+		return a+b;
+	}
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("task 1");
+        
         
     }
 }
