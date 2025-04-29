a80f2f27712f8f0233682cc959326c5b9d731cfb
Jatintiwari3341 <tripathijatin319@gmail.com>
2025-04-29 23:27:20 +0530

    task updated

--- Proj01/src/main/java/com/nt/main/App.java
+++ Proj01/src/main/java/com/nt/main/App.java
@@ -6,7 +6,7 @@ package com.nt.main;
 public class App {
 	
 	public double Sum(double a,double b) {
-		return a+b;
+		return (a+b)+0;
 	}
     public static void main(String[] args) {
         System.out.println("Hello World!");
