53576caf5b16b53e0e0919d2e582d7f0789acc35
Jatintiwari3341 <tripathijatin319@gmail.com>
2025-05-01 00:16:13 +0530

    task

--- Proj01/src/main/java/com/nt/main/App.java
+++ Proj01/src/main/java/com/nt/main/App.java
@@ -13,12 +13,9 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
+         System.out.println("task 1");
         
-
-        System.out.println("task 1");
-        
-        System.out.println("task 2");
-        System.out.println("task 3 added by tl ");
+     
         
     }
 }
