4cdbb45ae093b89ff17562fdfcc7297b0c173465
Jatintiwari3341 <tripathijatin319@gmail.com>
2025-05-01 00:06:15 +0530

    vv

--- Proj01/src/main/java/com/nt/main/App.java
+++ Proj01/src/main/java/com/nt/main/App.java
@@ -12,6 +12,7 @@ public class App {
 	}
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        //Update the task1
         System.out.println("task 1");
         
         System.out.println("task 2");
