b93825044477046bff8e5fc32523bfc066c5e46f
Jatintiwari3341 <tripathijatin319@gmail.com>
2025-05-01 00:39:11 +0530

    task 1

--- Proj01/src/main/java/com/nt/main/App.java
+++ Proj01/src/main/java/com/nt/main/App.java
@@ -12,24 +12,8 @@ public class App {
 	}
     public static void main(String[] args) {
         System.out.println("Hello World!");
-<<<<<<< HEAD
+
         System.out.println("task 1");
-=======
 
-
-<<<<<<< HEAD
-        System.out.println("task 1");
-        
-
-
-
-         System.out.println("task 2");   
-=======
-
-
->>>>>>> branch 'master' of https://github.com/Jatintiwari3341/FullStackRepo.git
-
-
->>>>>>> branch 'master' of https://github.com/Jatintiwari3341/FullStackRepo.git
     }
 }
