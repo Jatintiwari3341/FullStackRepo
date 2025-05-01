7aec7277083093defb7cac904e498dc7b5561452
Jatintiwari3341 <tripathijatin319@gmail.com>
2025-05-02 00:34:51 +0530

    task added by tl

---
+++ Proj02/src/main/java/com/jt/main/App.java
@@ -0,0 +1,11 @@
+package com.jt.main;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("task 1");
+    }
+}
