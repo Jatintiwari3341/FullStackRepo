66bc8749af9c5e5bbbb3168c78581a0580652a33
Jatintiwari3341 <tripathijatin319@gmail.com>
2025-05-02 00:50:21 +0530

    task added by dev1

--- Proj02/src/main/java/com/jt/main/App.java
+++ Proj02/src/main/java/com/jt/main/App.java
@@ -4,6 +4,9 @@ package com.jt.main;
  * Hello world!
  */
 public class App {
+	public int sum(int a,int b) {
+		return a+b;
+	}
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("task 1");
