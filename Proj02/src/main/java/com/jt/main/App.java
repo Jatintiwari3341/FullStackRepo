980ab3b788e51bae1e5db4713fd0812a040240f1
Jatintiwari3341 <tripathijatin319@gmail.com>
2025-05-02 01:02:11 +0530

    tt

--- Proj02/src/main/java/com/jt/main/App.java
+++ Proj02/src/main/java/com/jt/main/App.java
@@ -5,11 +5,7 @@ package com.jt.main;
  */
 public class App {
 	public int sum(int a,int b) {
-<<<<<<< HEAD
 		return (a+b);
-=======
-		return (a+b)+0;
->>>>>>> branch 'master' of https://github.com/Jatintiwari3341/FullStackRepo.git
 	}
     public static void main(String[] args) {
         System.out.println("Hello World!");
