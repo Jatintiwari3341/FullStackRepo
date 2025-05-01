fdcb1405dcf11c72d3d0f31506bb91d27b121e21
Jatintiwari3341 <tripathijatin319@gmail.com>
2025-05-02 00:43:07 +0530

    Merge branch 'master' of https://github.com/Jatintiwari3341/FullStackRepo.git

--- Proj_01/src/main/java/com/nt/main/App.java
+++ Proj_01/src/main/java/com/nt/main/App.java
@@ -5,9 +5,7 @@ package com.nt.main;
  */
 public class App {
 	
-	public int sum(int a,int b) {
-		return a=b;
-	}
+	
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
